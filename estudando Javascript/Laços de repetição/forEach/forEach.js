373b5cabd7caf0573874d47a839fc68cdc1969fa
Gustavo Miguel Oliveira Biscaro <gustavo.biscaro@gmail.com>
2023-10-20 23:14:44 -0300

    forEach

--- estudando Javascript/Laços de repetição/forEach/forEach.js
+++ estudando Javascript/Laços de repetição/forEach/forEach.js
@@ -1,3 +1,7 @@
+// EXEMPLO 1
+//DISPARO AUTOMÁTICO DE EMAIL PARA UMA LISTA DE CONTATO
+
+/*
 let listaAprovados = [
     'gustavinho@gloob.com',
     'merlyn@plataform.com',
@@ -15,6 +19,78 @@ listaAprovados.forEach((item, posicao, array) => { //1ª parâmetro é o item, o
     console.log(`Sua posição é a de: ${posicao}`);
     console.log(`A quantidade de pessoas no processo seletivo é: ${array.length}`)
 })
+*/
+
+// EXEMPLO 2
+// ARRAY SIMPLES
+/*
+let pessoas = ['Gustavo', 'Maria', 'José']
+pessoas.forEach((valorAtual, indice, arrayCompleto) => {
+    console.log(indice, valorAtual, arrayCompleto);
+
+})
+*/
+
+// EXEMPLO 3 
+// ARRAY DE OBJETOS - MOSTRAR NA TELA
+
+/*let produtos = [
+    {
+        nomeProduto: 'Celular',
+        valorProduto: 1200.00,
+        porcentagemDesconto: 0.20
+    },
+
+    {
+        nomeProduto: 'Desktop',
+        valorProduto: 3000.00,
+        porcentagemDesconto: 0.30,
+    },
+
+    {
+        nomeProduto: 'Televisao',
+        valorProduto: 3500.00,
+        porcentagemDesconto: 0.35
+    }
+];
+
+produtos.forEach((produtoAtual) => {
+    console.log(produtoAtual.nomeProduto, + '' + produtoAtual.valorProduto); //produtoAtual imprimi o array do objeto, produtoAtual.nomeProduto imprime apenas o nome
+});
+*/
 
 
 
+
+//EXEMPLO 4
+// CALCULAR O DESCONTO
+
+let produtos = [
+    {
+        nomeProduto: 'Celular',
+        valorProduto: 1200.00,
+        porcentagemDesconto: 0.20
+    },
+
+    {
+        nomeProduto: 'Desktop',
+        valorProduto: 3000.00,
+        porcentagemDesconto: 0.30,
+    },
+
+    {
+        nomeProduto: 'Televisao',
+        valorProduto: 3500.00,
+        porcentagemDesconto: 0.35
+    }
+];
+
+produtos.forEach((produtoAtual) => {
+    let valorDesconto = produtoAtual.valorProduto * produtoAtual.porcentagemDesconto;
+    let valorFinal = produtoAtual.valorProduto - valorDesconto;
+    console.log('\n');
+    console.log(produtoAtual.nomeProduto + ' | Valor: R$' + produtoAtual.valorProduto);
+    console.log("Desconto: R$" + valorDesconto);
+    console.log("Valor Final: R$" + valorFinal);
+    console.log('\n');
+})
--- estudando Javascript/Laços de repetição/forEach/leia.md
+++ estudando Javascript/Laços de repetição/forEach/leia.md
@@ -1,5 +1,7 @@
 # O que é forEach no JavaScript?
 Para Objetos do tipo array e serve para que você execute um bloco de códigos para cada um dos elementos do tipo array. E o array é um objeto para que você armazene outros objetos em forma de lista.
 
+ForEach percorre arrays, ele não retorna valor, seu uso é mais indicado é quando você pretende manipular seu array e fazer algum cálculo dentro dele.
+
 ## Analogia
 Caso você seja aprovado em um processo seletivo, enviaremos um email para todos os aprovados deste processo, numa lista de aprovados. O array seria a lista. E podemos usar o forEach para o processo de envio de email.
